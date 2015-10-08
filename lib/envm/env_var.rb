ed8315dd8676be0d036bccb3c864210ea3d2775a
Chris Ledet <me@chrisledet.com>
2015-10-08 09:10:29 -0700

    Rename ivar on env var for clarity

--- lib/envm/env_var.rb
+++ lib/envm/env_var.rb
@@ -2,24 +2,23 @@ require "envm/config"
 
 module Envm
   class EnvVar
-    attr_accessor :name, :description, :default_value, :required
+    attr_accessor :name, :description, :default_value, :required_environments
 
     def initialize(name:, description: nil, default_value: nil, required: [])
       self.name = name
       self.description = description
       self.default_value = default_value
-      self.required = []
 
       if required.respond_to?(:include?)
-        self.required = required
+        self.required_environments = required
       else
-        self.required = []
-        self.required << DEFAULT_ENV if required
+        self.required_environments = []
+        self.required_environments << DEFAULT_ENV if required
       end
     end
 
     def value
-      if required?(Config.environment)
+      if required_environments.include?(Config.environment)
         system_value or fail(NotSetError, "'#{name}' environment variable was required but set on system.")
       else
         default_value || system_value
@@ -28,12 +27,6 @@ module Envm
 
     def system_value
       ENV[name]
-    end
-
-    private
-
-    def required?(env)
-      required.include?(env)
     end
   end
 end
